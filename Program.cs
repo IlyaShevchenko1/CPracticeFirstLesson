25434a6dd12152458e460489f5a78000203ec98e
Ilya Shevchenko <6BLK@mail.ru>
2023-07-22 15:08:46 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿ /* int intNumber = 25; //Целые числа
+double doubleNumber =12.5; // Дробные числа
+string "text";
+bool logicVar = true;
+*/
+int num = 5;
+int num1 = 10;
+int num2 = 15;
+Console.WriteLine ("My number is " + num);
+Console.WriteLine ("My numbers are " + num1 + " and " + num2 " and it is good"); 
