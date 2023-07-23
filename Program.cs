cf096d5c99c92c1a5e7db38feaea4fb24d912913
Ilya Shevchenko <6BLK@mail.ru>
2023-07-23 23:37:35 +0300

    Add some theory before practice

--- Program.cs
+++ Program.cs
@@ -3,8 +3,17 @@ double doubleNumber =12.5; // Дробные числа
 string "text";
 bool logicVar = true;
 */
+
+/*
 int num = 5;
 int num1 = 10;
 int num2 = 15;
 Console.WriteLine ("My number is " + num);
-Console.WriteLine ("My numbers are " + num1 + " and " + num2 + " and it is good"); 
+Console.WriteLine ("My numbers are " + num1 + " and " + num2 + " and it is good");
+Console.WriteLine ($"My numbers are {num1} and {num2} and it is good");
+*/
+/*
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Your number is " + num);
+*/
